825b9483f7cafbb6ad56c3ccb1d1862699796a23
Kai Ho CHAN <Crashes.John@gmail.com>
2022-12-21 16:47:25 +0800

    Final: final product of mission control

--- controller.py
+++ controller.py
@@ -6,6 +6,7 @@ from tf.transformations import euler_from_quaternion
 from geometry_msgs.msg import Point, Twist
 from math import atan2
 from datetime import datetime
+from time import sleep
 
 x = 0.0
 y = 0.0 
@@ -24,7 +25,7 @@ def newOdom(msg):
 
 rospy.init_node("speed_controller")
 
-sub = rospy.Subscriber("/odometry/filtered", Odometry, newOdom)
+sub = rospy.Subscriber("/odom", Odometry, newOdom)
 pub = rospy.Publisher("/cmd_vel", Twist, queue_size = 1)
 
 speed = Twist()
@@ -35,26 +36,60 @@ Turned = False
 Finish = False
 
 goal = Point()
-goal.x = 0
-goal.y = 7.2
+goal.x = 2
+goal.y = 0
 
 goal2 = Point()
-goal2.x = 8
-goal2.y = 8
+goal2.x = 2
+goal2.y = 2
 
 time_start = datetime.now()
 
-def CombinedMovement(ang_to_goal, phi, speed_ang_z):
+def CombinedMovement(ang_to_goal, phi):
         if abs(ang_to_goal - phi) > 0.1:
-                speed.linear.x = 5.0
-                speed.angular.z = speed_ang_z
+                speed.linear.x = 0.0
+                speed.angular.z = 4.5
         else:
-                speed.linear.x = 7.0
+                speed.linear.x = 1.5
                 speed.angular.z = 0.0
 
 def StopMovement():
         speed.linear.x = 0.0
         speed.angular.z = 0.0
+
+def AutoController():
+        if x < 2.25:
+                speed.linear.x = 1.5
+                speed.angular.z = 0.0
+        elif theta <= 1.3:
+                speed.linear.x = 0.0
+                speed.angular.z = 4.2
+        else:
+                if y < 2:
+                        speed.linear.x = 1.5
+                        speed.angular.z = -1.5
+                elif y < 4:
+                        speed.linear.x = 1.5
+                        speed.angular.z = 0.0
+                else:
+                        StopMovement()
+
+def TimerController():
+        if (datetime.now() - time_start).total_seconds() < 3.5:
+                speed.linear.x = 1.5
+                speed.angular.z = 0.0
+        elif (datetime.now() - time_start).total_seconds() < 4.0:
+                StopMovement()
+        elif (datetime.now() - time_start).total_seconds() < 6.25:
+                speed.linear.x = 0.0
+                speed.angular.z = 4.5
+        elif (datetime.now() - time_start).total_seconds() < 7.25:
+                StopMovement()
+        elif (datetime.now() - time_start).total_seconds() < 10.75:
+                speed.linear.x = 1.5
+                speed.angular.z = 0.0
+        else:
+                StopMovement()
 
 while not rospy.is_shutdown():
         inc_x = goal.x -x
@@ -74,39 +109,22 @@ while not rospy.is_shutdown():
         
         """
         if Turned == False:
-                CombinedMovement(angle_to_goal, theta, 3)
+                CombinedMovement(angle_to_goal, theta)
                 if abs(inc_y) < 0.075:
                         Turned = True
                         StopMovement()
         elif Turned == True:
-                CombinedMovement(angle_to_goal2, theta, -3)
+                CombinedMovement(angle_to_goal2, theta)
                 if abs(inc_x2) < 0.15:
                         Finish = True
                         StopMovement()
         elif Finish == True:
                 StopMovement()
         """
+        print(f"{x}, {y}")
+        AutoController()
 
-        if (datetime.now() - time_start).total_seconds() < 3.5:
-                speed.linear.x = 1.5
-                speed.angular.z = 0.0
-        elif (datetime.now() - time_start).total_seconds() < 4.0:
-                speed.linear.x = 0.0
-                speed.angular.z = 0.0
-        elif (datetime.now() - time_start).total_seconds() < 6.25:
-                speed.linear.x = 0.0
-                speed.angular.z = 4.5
-        elif (datetime.now() - time_start).total_seconds() < 7.25:
-                speed.linear.x = 0.0
-                speed.angular.z = 0.0
-        elif (datetime.now() - time_start).total_seconds() < 10.75:
-                speed.linear.x = 1.5
-                speed.angular.z = 0.0
-        else:
-                speed.linear.x = 0.0
-                speed.angular.z = 0.0
-                
-        
+        #TimerController()
         
         # This statement prevents the car from self-rotating when it arrive destination
         #if inc_x < 0.1 and inc_y < 0.1:
